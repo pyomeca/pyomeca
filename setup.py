20b4f78eeddb30e3b68ec6bcf54338481938efac
Pariterre <pariterre@hotmail.com>
2018-01-22 16:11:30 -0500

    First commit of PyoMeca

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyomeca',
+    version='2018.01.22',
+    packages=['pyomeca', 'pyomeca.math'],
+    url='pyomeca.org',
+    license='MIT license',
+    author='Pariterre & Romain Martinez',
+    author_email='',
+    description='Toolbox for biomechanics analysis'
+)
