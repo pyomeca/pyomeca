701ad288bda703cf5076d635176ee8974a3f31b5
Romain Martinez <martinez.staps@gmail.com>
2018-04-14 11:39:05 -0400

    "-# -*- coding: utf-8 -*-" is not necessary in python 3

--- pyomeca/show/vtk.py
+++ pyomeca/show/vtk.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 
 Visualization toolkit in pyomeca
--- pyomeca/types/__init__.py
+++ pyomeca/types/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 __all__ = [
     'frame_dependent', 'markers', 'analogs', 'generalized_coordinates', 'rototrans', 'mesh'
 ]
