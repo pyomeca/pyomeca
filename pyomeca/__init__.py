bd86a17e092ba5055e9591dc79d8b8a11cdf621a
romain <martinez.staps@gmail.com>
2018-10-03 13:41:05 -0400

    versioneer

--- pyomeca/__init__.py
+++ pyomeca/__init__.py
@@ -3,3 +3,7 @@ from .markers import *
 from .analogs import *
 from .generalized_coordinates import *
 from .rototrans import *
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
