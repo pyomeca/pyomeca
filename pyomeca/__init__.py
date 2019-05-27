9b4330226853c1a0e14ad04775e0a6c6971ef3ef
Pariterre <pariterre@hotmail.com>
2019-05-27 15:52:46 -0400

    Removed versions

--- pyomeca/__init__.py
+++ pyomeca/__init__.py
@@ -4,6 +4,3 @@ from .analogs import *
 from .generalized_coordinates import *
 from .rototrans import *
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
--- setup.py
+++ setup.py
@@ -1,23 +1,13 @@
-import yaml
 from setuptools import setup
-
-import versioneer
-
-with open("environment.yml", 'r') as stream:
-    out = yaml.load(stream)
-    requirements = out['dependencies'][1:]  # we do not return python
 
 setup(
     name='pyomeca',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
     description="Pyomeca is a python library allowing to carry out a complete biomechanical analysis; in a simple, logical and concise way",
     author="Romain Martinez & Benjamin Michaud",
     author_email='martinez.staps@gmail.com',
     url='https://github.com/pyomeca/pyomeca',
     license='Apache 2.0',
     packages=['pyomeca'],
-    install_requires=requirements,
     keywords='pyomeca',
     classifiers=[
         'Programming Language :: Python :: 3.6',
