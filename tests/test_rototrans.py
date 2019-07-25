e7f94d59ef3e7e8d6ea6878d643490966139b504
Pariterre <pariterre@hotmail.com>
2019-07-25 15:29:44 +0200

    Adjusted the tests for RotoTrans

--- tests/test_rototrans.py
+++ tests/test_rototrans.py
@@ -4,14 +4,14 @@ Test for euler to rot and rot to euler
 import numpy as np
 import pytest
 
-from pyomeca import RotoTrans
+from pyomeca import RotoTrans, FrameDependentNpArray
 
 # Define all the possible angle_sequence to tests
 SEQ = ["x", "y", "z", "xy", "xz", "yx", "yz", "zx", "zy", "xyz", "xzy", "yxz", "yzx", "zxy", "zyx", "zyzz"]
 # If the difference between the initial and the final angles are less than epsilon, tests is success
-EPSILON = 1e-14
+EPSILON = 1e-12
 # Define some random data to tests
-ANGLES = np.random.rand(40, 1)
+ANGLES = FrameDependentNpArray(np.random.rand(40, 1, 100))
 
 
 @pytest.mark.parametrize('seq', SEQ)
@@ -20,10 +20,26 @@ ANGLES = np.random.rand(40, 1)
 def test_euler2rot_rot2euler(seq, angles, epsilon):
     """Test euler to RotoTrans and RotoTrans to euler."""
     # Extract the right amount of angle relative to sequence length
-    angles_to_test = angles[0:len(seq)]
+    if seq != "zyzz":
+        angles_to_test = angles[0:len(seq), :, :]
+    else:
+        angles_to_test = angles[0:3, :, :]
     # Get a RotoTrans from euler angles
     p = RotoTrans(angles=angles_to_test, angle_sequence=seq)
     # Get euler angles back from RotoTrans
     a = p.get_euler_angles(angle_sequence=seq)
 
     np.testing.assert_array_less((a - angles_to_test).sum(), epsilon)
+
+
+@pytest.mark.parametrize('angles', [ANGLES])
+def test_rt_mean(angles):
+    seq = "xyz"
+    angles_to_test = angles[0:3, :, :]
+    p = RotoTrans(angles=angles_to_test, angle_sequence=seq)
+    angles_mean = p.mean().get_euler_angles(seq)
+
+    # Test the difference with a very relax tolerance since angles_to_compare is false by definition
+    # but should not be too far most of the time
+    angles_to_compare = p.get_euler_angles(angle_sequence=seq).mean()
+    np.testing.assert_array_less((angles_mean - angles_to_compare).sum(), 1e-1)
