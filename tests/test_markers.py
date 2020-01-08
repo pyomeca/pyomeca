0c26c3116c06c0f084933765ae3362f787a5ecc4
pariterre <pariterre@hotmail.com>
2020-01-08 11:12:24 -0500

    simplified the computation of the norm for the markers and added a test

--- tests/test_markers.py
+++ tests/test_markers.py
@@ -6,7 +6,7 @@ import numpy as np
 from pyomeca import RotoTrans, FrameDependentNpArray, Markers3d
 
 
-def test_rotate_marker():
+def test_rotate():
     # Create random RotoTrans matrices and markers positions
     nb_frames = 100
     nb_markers = 10
@@ -26,3 +26,22 @@ def test_rotate_marker():
 
     # Compare the two
     np.testing.assert_almost_equal(computed_rot, expected_rot, decimal=10)
+
+
+def test_norm():
+    # Create some markers positions
+    nb_frames = 100
+    nb_markers = 10
+    m = Markers3d(np.random.rand(3, nb_markers, nb_frames))
+
+    # Compute the norm by hand
+    expected_norm = np.ndarray((1, nb_markers, nb_frames))
+    for i in range(nb_markers):
+        for j in range(nb_frames):
+            expected_norm[:, i, j] = np.sqrt(m[0:3, i, j].squeeze().dot(m[0:3, i, j]))
+
+    # Compute the norm using Pyomeca
+    computed_norm = m.norm()
+
+    # Compare the two
+    np.testing.assert_almost_equal(computed_norm, expected_norm, decimal=10)
