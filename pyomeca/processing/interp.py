68f091e7589d0d25fe03f6aef29dacef27b3d689
eve-mac <eve.charbonneau.1@umontreal.ca>
2025-03-27 18:24:22 +0400

    reverted

--- pyomeca/processing/interp.py
+++ pyomeca/processing/interp.py
@@ -25,28 +25,4 @@ def time_normalize(
 
 
 def interpolate_missing_data(array: xr.DataArray) -> xr.DataArray:
-
-    def interpolate(vector: np.ndarray) -> np.ndarray:
-        interpolated_vector = np.zeros_like(vector)
-        bad_indexes = np.isnan(vector)
-        good_indexes = np.logical_not(bad_indexes)
-        good_vector = vector[good_indexes]
-        interpolated_vector[good_indexes] = vector[good_indexes]
-        interpolated = np.interp(np.nonzero(np.array(bad_indexes))[0], np.nonzero(np.array(good_indexes))[0], np.array(good_vector))
-        interpolated_vector[bad_indexes] = interpolated
-        return interpolated_vector
-
-    interpolated_array = np.zeros_like(array)
-    if len(array.shape) == 2:
-        for i_shape0 in range(array.shape[0]):
-            interpolated_array[i_shape0, :] = interpolate(array[i_shape0, :])
-    elif len(array.shape) == 3:
-        for i_shape0 in range(array.shape[0]):
-            for i_shape1 in range(array.shape[1]):
-                interpolated_array[i_shape0, i_shape1, :] = interpolate(array[i_shape0, i_shape1, :])
-    else:
-        raise NotImplementedError("Only 2D and 3D arrays are supported yet for interpolate_missing_data.")
-
-    new_array = array.copy()
-    new_array.values = interpolated_array[:, :]
-    return new_array
+    return array.interpolate_na(dim="time", method="linear")
