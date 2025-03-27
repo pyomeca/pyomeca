4382c5a30dd9e43f6b86aff7a1b4c9d15199c5a3
eve-mac <eve.charbonneau.1@umontreal.ca>
2025-03-27 11:49:53 +0400

    interpolate missing data

--- pyomeca/processing/interp.py
+++ pyomeca/processing/interp.py
@@ -22,3 +22,19 @@ def time_normalize(
             first_last_time = (array.time[0], array.time[-1])
         time_vector = np.linspace(first_last_time[0], first_last_time[1], n_frames)
     return array.interp(time=time_vector)
+
+
+def interpolate_missing_data(array: xr.DataArray) -> xr.DataArray:
+
+    interpolated_array = np.zeros_like(array)
+    for i in range(array.shape[0]):
+        bad_indexes = np.isnan(array[i, :])
+        good_indexes = np.logical_not(bad_indexes)
+        good_array = array[i, good_indexes]
+        interpolated_array[i, good_indexes] = array[i, good_indexes]
+        interpolated = np.interp(np.nonzero(np.array(bad_indexes))[0], np.nonzero(np.array(good_indexes))[0], np.array(good_array))
+        interpolated_array[i, bad_indexes] = interpolated
+
+    new_array = array.copy()
+    new_array.values = interpolated_array[:, :]
+    return new_array
